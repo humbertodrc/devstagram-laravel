e854af867e1a2f5347966d769f36eaccd0991679
unknown <humbertorivero9@gmail.com>
2022-07-31 03:09:16 -0300

    Feat layouts

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Nosotros</title>
+@section('titulo')
+    Nosotros
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body>
-        <nav>
-          <a href="/">Principal</a>
-          <a href="/nosotros">Nosoros</a>
-          <a href="/tienda">Tienda</a>
-        </nav>
-        <h1>Nosotros</h1>
-    </body>
-</html>
+@section('contenido')
+    contenido de nosotros
+@endsection
