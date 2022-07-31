6c5383085ee1af1d10fdb95c905c9ae4fd519fbf
unknown <humbertorivero9@gmail.com>
2022-07-31 00:31:02 -0300

    Add routes test

---
+++ resources/views/otra-vista.blade.php
@@ -0,0 +1,1 @@
+<h1>Otra vista</h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/otra', function () {
+    return view('otra-vista');
+});
